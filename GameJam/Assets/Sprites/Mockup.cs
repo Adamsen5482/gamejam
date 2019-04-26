41e5d9f124bc7c44e45ab895c9b614abc00e73f3
DESKTOP-HNVPF6P\Mikkel <mikkel@sirenix.net>
2019-04-26 19:50:45 +0200

    Basic scripts.

---
+++ GameJam/Assets/Sprites/Mockup.cs
@@ -0,0 +1,49 @@
+﻿using Sirenix.OdinInspector;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum Role
+{
+    Error = 0,
+    Murder,
+    Ghost,
+    Accomplice,
+    Detective,
+}
+
+public class PlayerInfo
+{
+    public string Name;
+    public Color Color;
+    public Role Role;
+}
+
+public static class PlayerList
+{
+    public static List<PlayerInfo> Players = new List<PlayerInfo>();
+}
+
+[Serializable]
+public class Weapon
+{
+    public string Name;
+    [PreviewField, PropertyOrder(-1)]
+    public Sprite Icon;
+}
+
+public static class GameSetup
+{
+    public static void SetupNewGame(List<PlayerInfo> players)
+    {
+        PlayerList.Players.Clear();
+
+        // Assign roles
+    }
+
+
+    private static PlayerInfo PickRandom(List<PlayerInfo> players)
+    {
+        return players[UnityEngine.Random.Range(0, players.Count)];
+    }
+}
---
+++ GameJam/Assets/Sprites/MurderWeaponsList.cs
@@ -0,0 +1,9 @@
+﻿using Sirenix.OdinInspector;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MurderWeaponsList : ScriptableObject
+{
+    [TableList]
+    public List<Weapon> Weapons;
+}
