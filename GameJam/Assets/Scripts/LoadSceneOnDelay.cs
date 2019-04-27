8fd1e7d0f6ad90114bd0dca46d8e3c58740d2229
DESKTOP-HNVPF6P\Mikkel <mikkel@sirenix.net>
2019-04-27 22:28:17 +0200

    SPLASHED

---
+++ GameJam/Assets/Scripts/LoadSceneOnDelay.cs
@@ -0,0 +1,47 @@
+﻿using Sirenix.OdinInspector;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LoadSceneOnDelay : MonoBehaviour
+{
+    public Image Fade;
+
+    [FilePath(Extensions = ".unity")]
+    public string ScenePath;
+
+    public float FadeTime;
+
+    [Button, DisableInEditorMode]
+    public void LoadScene()
+    {
+        this.StartCoroutine(this.LoadSceneRoutine());
+    }
+
+    private IEnumerator LoadSceneRoutine()
+    {
+        float time = Time.unscaledTime;
+        float step = 1f / this.FadeTime;
+
+        var c = this.Fade.color;
+        c.a = 0f;
+
+        while ((Time.unscaledTime - time) < this.FadeTime)
+        {
+            c = this.Fade.color;
+            c.a = Mathf.Lerp(0f, 1f, (Time.unscaledTime - time) / this.FadeTime);
+            this.Fade.color = c;
+            yield return null;
+        }
+
+        c = this.Fade.color;
+        c.a = 1f;
+        this.Fade.color = c;
+
+        //yield return new WaitForSeconds(0.05f);
+        yield return null;
+        yield return null;
+        SceneManager.LoadSceneAsync(this.ScenePath);
+    }
+}
---
+++ GameJam/Assets/Scripts/RotateSelf.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class RotateSelf : MonoBehaviour
+{
+    public Vector3 Axis;
+    public float Speed;
+
+    private float angle;
+
+    private void Update()
+    {
+        this.angle += this.Speed * Time.smoothDeltaTime;
+        this.transform.localRotation = Quaternion.AngleAxis(this.angle, this.Axis.normalized);
+    }
+}
